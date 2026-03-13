8c4a7478dcad591626e4337b4737d9d00b1850a9
Christopher Hastings <156140470+christopherhasti@users.noreply.github.com>
2026-03-13 23:21:30 +0000

    Checkpoint 1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"hello": "world"}
+# Set up the templates directory
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/browse", response_class=HTMLResponse)
+async def browse(request: Request):
+    return templates.TemplateResponse("browse.html", {"request": request})
+
+@app.get("/add", response_class=HTMLResponse)
+async def add_recipe(request: Request):
+    return templates.TemplateResponse("add.html", {"request": request})
+
+@app.get("/recipes/{id}", response_class=HTMLResponse)
+async def recipe_detail(request: Request, id: int):
+    # Conditionally retrieves based on recipe ID in the future
+    return templates.TemplateResponse("detail.html", {"request": request, "id": id})
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
